2acdc4ae2db804758bfa8fd74a95732714f78822
Christian Stefanescu <chris@0chris.com>
2020-06-01 23:07:25 +0200

    Copy to clipboard by default

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use std::path::Path;
 use lazy_static::lazy_static;
 
 use anyhow::Error;
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use secrecy::Secret;
 use structopt::StructOpt;
 
@@ -33,7 +35,13 @@ enum Opt {
     /// List all known entries
     List,
     /// Decrypt and show an entry
-    Show { entry: String },
+    Show {
+        entry: String,
+
+        #[structopt(long)]
+        /// Print the password instead of copying it to the clipboard
+        on_screen: bool,
+    },
 }
 
 fn encrypt(plaintext: &[u8], passphrase: String) -> Result<Vec<u8>, Error> {
@@ -47,10 +55,10 @@ fn encrypt(plaintext: &[u8], passphrase: String) -> Result<Vec<u8>, Error> {
     Ok(encrypted)
 }
 
-fn decrypt(encrypted: &[u8], passphrase: String) -> Result<Vec<u8>, Error> {
+fn decrypt(encrypted: &[u8], passphrase: String, clipboard: bool) -> Result<Vec<u8>, Error> {
     let decryptor = match age::Decryptor::new(&encrypted[..])? {
         age::Decryptor::Passphrase(d) => d,
-        _ => unreachable!(),
+        age::Decryptor::Recipients(..) => unreachable!(),
     };
 
     let mut decrypted = vec![];
@@ -60,6 +68,14 @@ fn decrypt(encrypted: &[u8], passphrase: String) -> Result<Vec<u8>, Error> {
         if bytes == 0 {
             break;
         }
+    }
+
+    if clipboard {
+        let mut ctx: ClipboardContext =
+            ClipboardProvider::new().expect("Failed to instantiate clipboard provider");
+        ctx.set_contents(String::from_utf8(decrypted)?)
+            .expect("Failed to copy to clipboard");
+        return Ok(vec![]);
     }
 
     Ok(decrypted)
@@ -96,15 +112,16 @@ fn init() -> Result<(), Error> {
     Ok(())
 }
 
-fn show(entry: &str) -> Result<(), Error> {
-    println!("Showing {}", entry);
+fn show(entry: &str, on_screen: bool) -> Result<(), Error> {
     let mut encrypted: Vec<u8> = vec![];
     let file = File::open(format!("{}/{}", STORAGE_DIR.clone(), entry))?;
     let mut buf = BufReader::new(file);
     buf.read_to_end(&mut encrypted)?;
     let passphrase = rpassword::prompt_password_stdout("Enter passphrase:")?;
-    let decrypted = decrypt(&encrypted, passphrase)?;
-    println!("{}", String::from_utf8(decrypted)?);
+    let decrypted = decrypt(&encrypted, passphrase, !on_screen)?;
+    if on_screen {
+        println!("{}", String::from_utf8(decrypted)?);
+    }
 
     Ok(())
 }
@@ -115,6 +132,6 @@ fn main() -> Result<(), Error> {
         Opt::New => new_entry(),
         Opt::List => list(),
         Opt::Init => init(),
-        Opt::Show { entry } => show(&entry),
+        Opt::Show { entry, on_screen } => show(&entry, on_screen),
     }
 }
