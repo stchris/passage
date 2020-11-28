3f9d0a8ce82e2f7bb26f1ff012fe3da9d10f58a3
Christian Stefanescu <chris@0chris.com>
2020-11-28 13:27:50 +0100

    Prompt to run init if storage file is not there
    Update CHANGELOG for fail-when-no-init

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,8 +1,18 @@
+use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
 fn passage() -> Command {
     Command::cargo_bin("passage").unwrap()
+}
+
+/// removes the storage file so we can start clean
+fn remove_entries() -> Result<()> {
+    let stdout = String::from_utf8(passage().arg("info").output()?.stdout)?;
+    let first_line = stdout.lines().next().unwrap();
+    let storage_file = first_line.split(':').last().unwrap().trim();
+    std::fs::remove_file(storage_file)?;
+    Ok(())
 }
 
 #[test]
@@ -33,4 +43,41 @@ fn info() {
                 .and(predicate::str::contains("\n").count(1).trim()),
         ),
     );
+}
+
+#[test]
+fn fail_list_no_init() {
+    remove_entries().unwrap_or_default();
+    passage()
+        .arg("list")
+        .assert()
+        .failure()
+        .stderr(predicate::str::starts_with(
+            "Error: storage not initialized, run `passage init`",
+        ));
+}
+
+#[test]
+fn fail_show_no_init() {
+    remove_entries().unwrap_or_default();
+    passage()
+        .arg("show")
+        .arg("foo")
+        .assert()
+        .failure()
+        .stderr(predicate::str::starts_with(
+            "Error: storage not initialized, run `passage init`",
+        ));
+}
+
+#[test]
+fn fail_new_no_init() {
+    remove_entries().unwrap_or_default();
+    passage()
+        .arg("new")
+        .assert()
+        .failure()
+        .stderr(predicate::str::starts_with(
+            "Error: storage not initialized, run `passage init`",
+        ));
 }
