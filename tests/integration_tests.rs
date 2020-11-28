7c0e775063128f411e0a6e69a7e6d50e816da045
Christian Stefanescu <chris@0chris.com>
2020-11-28 01:21:01 +0100

    Add some basic integration tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,35 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+fn passage() -> Command {
+    Command::cargo_bin("passage").unwrap()
+}
+
+#[test]
+fn sanity() {
+    passage()
+        .arg("--version")
+        .assert()
+        .success()
+        .stdout(predicate::str::starts_with("passage "));
+
+    passage()
+        .arg("--help")
+        .assert()
+        .success()
+        .stdout(
+            predicate::str::starts_with("passage ").and(predicate::str::contains(
+                "Password manager with age encryption",
+            )),
+        );
+}
+
+#[test]
+fn info() {
+    passage().arg("info").assert().success().stdout(
+        predicate::str::starts_with("Storage file: ").and(
+            predicate::str::contains("entries.toml.age")
+                .and(predicate::str::contains("\n").count(1).trim()),
+        ),
+    );
+}
